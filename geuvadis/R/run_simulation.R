591d40d599359c36618befb0fbd857b078be3794
warrenmcg <warren-mcgee@fsm.northwestern.edu>
2018-06-06 17:07:49 -0500

    make several changes to the simulation + changed the seed + changed the denominator used because of the seed change + changed the counts used for determining mean-dispersion relationship to the mean of the full dataset + changed the probabilities of differential expression (now 5% for small; 20% for the other two groups) + changed the probabilities of up-regulation for the small group (now consistently 50%) + changed the output directory + changed the location of the "ground truth" RData object, and the "oracles" RDS object. + changed the number of cores used to 8 because of the RAM demands

--- geuvadis/R/run_simulation.R
+++ geuvadis/R/run_simulation.R
@@ -1,19 +1,18 @@
 .libPaths(c("~/R_library", .libPaths()))
-
 fasta_file <- '../../annotation/Homo_sapiens.gencode.v25.fa'
 sleuth_file <- '../results/finn_sleuth.rds'
-dir.create('../sims', ignoreWarnings = T)
-outdir <- '../sims'
+outdir <- '../sims/isoform_5_5_15_387_1'
+dir.create(outdir, showWarnings = F)
 
 ### This denominator was chosen for two reasons:
 # 1) It is known to not be changing across all fifteen runs
 # 2) Among the other genes that are not changing across all runs, 
 #    it had the lowest average COV
 ###
-denom <- 'ENST00000466430.5' # AL627309.1-201 lincRNA
+denom <- 'ENST00000377532.7' # PER3-202
 
 ### This sample is the most deeply sequenced Finnish sample in the GEUVADIS dataset
-sample_index <- 48 # this is a control sample from the real dataset
+sample_index <- 'mean'
 
 ### Fifteen total simulation runs (see groups A-C below)
 num_runs <- 15
@@ -39,27 +38,30 @@ de_type <- 'normal'
 # Group A -- "few changes" (intended to match assumptions made by previous normalization methods)
 # Group B -- "many changes down" (intended to violate assumptions made by previous methods)
 # Group C -- "many changes up" (intended to violate assumptions made by previous methods)
+# Note: Groups B and C have the same probability
 ###
-de_probs <- c(rep(0.01, 5), rep(0.5, 5), rep(0.2, 5))
+de_probs <- c(rep(0.05, 5), rep(0.20, 10))
 
 ### probability of being up-regulated
 # Group A has 3 runs of 50/50, and one run each with a skew of 90% down or up, respectively
 # Group B has all 5 runs with 90% down
 # Group C has all 5 runs wtih 90% up
 ###
-dir_probs <- c(rep(0.5, 3), 0.1, 0.9, rep(0.1, 5), rep(0.9, 5))
+dir_probs <- c(rep(0.3, 5), rep(0.1, 5), rep(0.9, 5))
 
 ### Mean library size per sample
 # This is set to 25 million reads
 ###
-mean_lib_size <- 25*10^6
+mean_lib_size <- 30*10^6
 
 ### The seed for the full simulation
 # Used code in 'testing.R' to determine this number
 ###
-seed <- 387 # alternative option was 369
+seed <- 645175
 
 ### Launching the actual full simulation
+# On our machine, we need to use 8 cores because of the memory demands
+# 8 cores consumes about 100-110 GB of RAM (out of 128 on our machine)
 final_results <- absSimSeq::run_abs_simulation(fasta_file, sleuth_file, sample_index,
                                                outdir = outdir, denom = denom,
                                                num_runs = num_runs, num_reps = num_reps,
@@ -67,12 +69,12 @@ final_results <- absSimSeq::run_abs_simulation(fasta_file, sleuth_file, sample_i
                                                de_type = de_type, mean_lib_size = mean_lib_size,
                                                de_probs = de_probs, dir_probs = dir_probs,
                                                polyester_sim = T, gc_bias = gc_bias,
-                                               sleuth_save = T, num_cores = num_runs)
+                                               sleuth_save = T, num_cores = 8)
 
 oracles <- absSimSeq::extract_oracles(final_results)
 
 save(final_results, sample_index, de_probs, dir_probs, fasta_file,
      sleuth_file, outdir, gc_bias, num_runs, num_reps, denom,
      seed, de_type, de_levels, mean_lib_size, sample_index,
-     file = file.path(outdir, 'polyester_ground_truth.RData'))
-saveRDS(oracles, file = file.path(outdir, 'polyester_oracles.rds'))
+     file = file.path('../results', 'polyester_ground_truth.RData'))
+saveRDS(oracles, file = file.path(outdir, '../polyester_oracles.rds'))
