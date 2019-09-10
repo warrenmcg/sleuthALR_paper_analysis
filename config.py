2f8a3d68437f04f58db84a612988adb26d28bdec
Warren McGee <warren-mcgee@fsm.northwestern.edu>
2019-09-10 14:31:35 -0500

    remove superfluous HISAT, RESM, and OS code

--- config.py
+++ config.py
@@ -20,24 +20,12 @@ BIN = CONDA + '/bin'
 KALLISTO = BIN + '/kallisto'
 SALMON = BIN + '/salmon'
 
-# UPDATED_PATH = 'PATH=' + ':'.join([
-#     RSEM_PATH,
-#     getenv('PATH')
-#     ])
 UPDATED_PATH = 'PATH=' + BIN + ':$PATH'
 
 SAMTOOLS = BIN + '/samtools'
-
 PANDOC = BIN + '/pandoc'
 OPENSSL = BIN + '/openssl'
 FASTQ_DUMP = BIN + '/fastq-dump'
-
-# import os
-
-# os.environ['PATH'] = RSEM_PATH + ':' + os.environ['PATH']
-# print(RSEM_PATH + ':' + os.environ['PATH'])
-# os.environ['PATH'] = RSEM_PATH + ':' + os.environ['PATH']
-# print(type(os.environ['PATH']))
 
 ###
 # simulation wildcards
@@ -50,43 +38,20 @@ SIM_IDS = ['sample_' + format(i, '02d') for i in range(1,11)]
 ###
 TRANSCRIPTOME_NAME = 'Homo_sapiens.gencode.v25'
 TRANSCRIPTOME_FA = BASE + '/annotation/' + TRANSCRIPTOME_NAME + '.fa'
-TRANSCRIPTOME_GTF = BASE + '/annotation/' + TRANSCRIPTOME_NAME + '.gtf'
 SPIKEIN_FA = BASE + '/annotation/' + TRANSCRIPTOME_NAME + '.andErcc92.fa'
-
-# NOTE: bowtie creates files with this prefix
-BOWTIE_INDEX = BASE + '/index/' + TRANSCRIPTOME_NAME
 
 KALLISTO_INDEX = BASE + '/index/' + TRANSCRIPTOME_NAME + '.kidx'
 SALMON_INDEX = BASE + '/index/' + TRANSCRIPTOME_NAME + '.sidx'
 KALLISTO_SPIKEIN_INDEX = BASE + '/index/' + TRANSCRIPTOME_NAME + '.andErcc92.kidx'
 SALMON_SPIKEIN_INDEX = BASE + '/index/' + TRANSCRIPTOME_NAME + '.andErcc92.sidx'
 
-GENOME_NAME = 'Homo_sapiens.GRCh38.dna.primary_assembly.rel87'
-GENOME_FA = BASE + '/annotation/' + GENOME_NAME + '.fa'
-
-# STAR_DIRECTORY = BASE + '/index/star_' + TRANSCRIPTOME_NAME
-# STAR_INDEX = STAR_DIRECTORY + '/Genome'
-HISAT_INDEX = BASE + '/index/' + GENOME_NAME
-
-
-
-RSEM_ANNOTATION_DIR = '/'.join([
-    BASE,
-    'annotation',
-    TRANSCRIPTOME_NAME + '_rsem'])
-
+## Mouse annotations
 MOUSE_NAME = 'Mus_musculus.gencode.vM12'
 MOUSE_TRANSCRIPTOME_FA = BASE + '/annotation/' + MOUSE_NAME + '.fa'
-MOUSE_TRANSCRIPTOME_GTF = BASE + '/annotation/' + MOUSE_NAME + '.gtf'
-
 MOUSE_KALLISTO_INDEX = BASE + '/index/' + MOUSE_NAME + '.kidx'
 MOUSE_SALMON_INDEX = BASE + '/index/' + MOUSE_NAME + '.sidx'
 
-MOUSE_GENOME_NAME = 'Mus_musculus.GRCm38.dna.primary_assembly.rel87'
-MOUSE_GENOME_FA = BASE + '/annotation/' + MOUSE_GENOME_NAME + '.fa'
-
-MOUSE_HISAT_INDEX = BASE + '/index/' + MOUSE_GENOME_NAME
-
+## Yeast annotations
 YEAST_NAME = 'ASM294v2.pombase.all'
 YEAST_ANNOS = BASE + '/annotation/' + YEAST_NAME + '_annos.txt'
 YEAST_TRANSCRIPTOME_FA = BASE + '/annotation/' + YEAST_NAME + '.fa'
